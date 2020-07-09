6a9d24697f0abf55398bf3557fb352a41682ed2c
Dazzy Mlv <23006212+DazzyMlv@users.noreply.github.com>
2020-07-09 11:57:37 +0100

    refactored

--- src/age-calc.c
+++ src/age-calc.c
@@ -1,14 +1,34 @@
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    float code = 10.5;
 
-    int year_of_birth;
-    year_of_birth = 2000;
+int get_current_year();
+int age_calc(int);
 
-    printf("%d %f", year_of_birth, code);
+int main()
+{
+    int year_of_birth = get_current_year();
 
+    while(true){
+        printf("Enter your year of birth or -1 to exit: ");
+        scanf("%d", &year_of_birth);
+
+        if(-1 == year_of_birth){
+            break;
+        }
+        printf("You are %d year(s) old.\n\n", age_calc(year_of_birth));
+    }
 
     return 0;
+}
+
+int get_current_year(){
+    // @TODO: get the current year automatically from the system using time.h
+    return 2020;
+}
+
+
+int age_calc(int year_of_birth){
+    return get_current_year() - year_of_birth;
 }
